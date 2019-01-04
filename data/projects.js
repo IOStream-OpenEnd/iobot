5720a087058fac5e3254e577986fac7605c0f46f
Matt Smith <mrmcchow@gmail.com>
2019-01-04 17:51:48 +0000

    Created project data file

---
+++ data/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+
+    {
+        "name": "magikm0ments",
+        "manager": "130519552361496577",
+        "lang": "Python",
+        "status": "Nearing completion",
+        // The Projects Channel ID
+        "link": "525564160365297694"
+    },
+    {
+        "name": "b3atsc0mpiler",
+        "manager": "527794552757813248",
+        "lang": "Python",
+        "status": "Nearing completion",
+        "link": "528276844118802435"
+    },
+    {
+        "name": "io-bot",
+        "manager": "179604866807627777",
+        "lang": "Javascript",
+        "status": "Just Started",
+        "link": "529351678466719755"
+    },
+    {
+        "name": "composiAI",
+        "manager": "525170427803467776",
+        "lang": "Python",
+        "status": "Just Started",
+        "link": "530047749329125406"
+    }
+
+];
+
+module.exports = projects;
