d98599af5d17e2aedae3b52e4eef4e14b1fd086a
Matt Smith <mrmcchow@gmail.com>
2019-01-04 17:37:39 +0000

    Updated onUserJoin

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,9 +6,9 @@ module.exports = async (client, member) => {
     client.welcomeNewMembers(member);
 
     // call `projects` command 
-    let chan = {}
-    chan["channel"] = await client.channels.get("524880564302381058")
-    client.commands.get('projects').run(client, chan)
+    // let chan = {}
+    // chan["channel"] = client.channels.get("524880564302381058")
+    // client.commands.get('projects').run(client, chan)
 
     // Set Bot Activity to correct user count
     client.setUserCount()
